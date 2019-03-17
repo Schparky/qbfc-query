47e8cd89a80098cd8aa84b128f70409205b1c4ba
Steve Schram <s27@gmx.us>
2019-03-16 21:13:10 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QBFC13Lib;
+
+namespace qbfc_query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListCustomers();
+        }
+
+        static public void ListCustomers()
+        {
+            QBSession Session = null;
+
+            try
+            {
+                Session = new QBSession();
+
+                IResponse response = Session.QueryCustomerList();
+
+                ICustomerRetList customerRetList = response.Detail as ICustomerRetList;
+                for (var i = 0; i < customerRetList.Count; i++)
+                {
+                    ICustomerRet customerRet = customerRetList.GetAt(i);
+                    Console.WriteLine(customerRet.FullName.GetValue());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine("Stack Trace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Exiting the application");
+            }
+        }
+    }
+}
